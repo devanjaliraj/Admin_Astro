e2d0667fc620c07bb4e2b8d273fd8c7b04e37195
devanjaliraj <anjalidevelopersveltosest@gmail.com>
2023-01-29 02:53:12 +0530

    build29/01/23

--- src00/axiosConfig.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  // baseURL: "http://35.154.86.59/api/admin",
-  baseURL: "http://13.233.228.168:8000",
-});
-
-export default instance;
